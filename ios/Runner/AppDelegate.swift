31f834e976889276279fad753143b356789f6163
MahmoudKirax677 <engm2056@gmail.com>
2024-08-15 13:49:02 +0300

    Comment Social

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,7 +8,13 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    // Initialize Google Maps with API Key
+    GMSServices.provideAPIKey("AIzaSyA8pIqHO4If7tMvIYIl60rFXI365Bfl3-I")
+    
+    // Register generated plugins
     GeneratedPluginRegistrant.register(with: self)
+
+    // Return the result of the super call
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
