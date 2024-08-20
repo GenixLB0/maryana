f3930df47372a77589c8cdb39ac642f60873f2ab
MahmoudKirax677 <engm2056@gmail.com>
2024-08-20 09:14:51 +0300

    add apple sign in

--- lib/app/modules/auth/controllers/auth_controller.dart
+++ lib/app/modules/auth/controllers/auth_controller.dart
@@ -1,21 +1,20 @@
 import 'dart:convert';
-
+import 'dart:io';
 import 'package:awesome_notifications/awesome_notifications.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:get/get.dart';
-import 'package:maryana/app/modules/auth/views/login_view.dart';
 import 'package:maryana/app/modules/global/config/constant.dart';
 import 'package:maryana/app/modules/global/model/model_response.dart';
-import 'package:maryana/app/modules/main/views/main_view.dart';
 import 'package:maryana/app/modules/services/api_service.dart';
-import 'package:maryana/main.dart';
 import 'package:dio/dio.dart' as dio;
 import 'package:shared_preferences/shared_preferences.dart';
-import 'dart:convert';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:firebase_auth/firebase_auth.dart' as authTest;
-
+import 'package:device_info_plus/device_info_plus.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:maryana/main.dart';
 import '../../../routes/app_pages.dart';
+import 'package:sign_in_with_apple/sign_in_with_apple.dart';
 
 class AuthController extends GetxController {
   var email = ''.obs;
@@ -36,115 +35,169 @@ class AuthController extends GetxController {
 
   authTest.FirebaseAuth auth = authTest.FirebaseAuth.instance;
 
-  // Existing methods...
+  String fcmToken = '';
+  String deviceId = '';
+  FirebaseMessaging? messaging;
+
+  @override
+  void onInit() {
+    super.onInit();
+    _requestNotificationPermissions();
+    _initFCMToken();
+    getDeviceId();
+    socialView.value = true;
+  }
+
+  Future<void> getDeviceId() async {
+    DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
+
+    if (Platform.isAndroid) {
+      AndroidDeviceInfo androidInfo = await deviceInfo.androidInfo;
+      deviceId = androidInfo.id; // المعرف الصحيح لجهاز Android
+      print('Android ID: $deviceId');
+    } else if (Platform.isIOS) {
+      IosDeviceInfo iosInfo = await deviceInfo.iosInfo;
+      deviceId = iosInfo.identifierForVendor!;
+      print('iOS ID: $deviceId');
+    }
+  }
+
+  Future<void> _requestNotificationPermissions() async {
+    // طلب الأذونات للإشعارات لكل من Android وiOS
+    AwesomeNotifications().isNotificationAllowed().then((isAllowed) {
+      if (!isAllowed) {
+        AwesomeNotifications().requestPermissionToSendNotifications();
+      }
+    });
+
+    if (GetPlatform.isIOS) {
+      await messaging!.requestPermission(
+        alert: true,
+        announcement: false,
+        badge: true,
+        carPlay: false,
+        criticalAlert: false,
+        provisional: false,
+        sound: true,
+      );
+    }
+  }
 
   Future<void> googleLogin() async {
     try {
       isLoading.value = true;
 
-      // Step 1: Initiate the Google Sign-In process
       final GoogleSignInAccount? googleUser = await googleSignIn.signIn();
       if (googleUser == null) {
-        // User canceled the sign-in process
         isLoading.value = false;
         print('User canceled the Google Sign-In process');
         return;
       }
 
-      // Step 2: Retrieve the authentication details
       GoogleSignInAuthentication googleAuth = await googleUser.authentication;
-
-      // Step 3: Retry authentication if idToken is null
       if (googleAuth.idToken == null) {
         googleAuth = await googleUser.authentication;
       }
 
-      // Step 4: Check if idToken is available
       if (googleAuth.idToken != null) {
         final providerToken = googleAuth.idToken;
 
-        // Step 5: Create a credential with the Google auth details
         final authTest.OAuthCredential credential =
             authTest.GoogleAuthProvider.credential(
           accessToken: googleAuth.accessToken,
           idToken: googleAuth.idToken,
         );
 
-        // Step 6: Sign in with the credential
         final authTest.UserCredential userCredential =
             await auth.signInWithCredential(credential);
 
-        // Step 7: Extract user information
         email.value = userCredential.user!.email ?? '';
         firstName.value = userCredential.user!.displayName ?? '';
-        lastName.value = ''; // Adjust as needed
+        lastName.value = ''; // يمكن تعديلها حسب الحاجة
 
-        // Step 8: Call the loginWithGoogle method with the provider token
-        loginWithGoogle(providerToken!);
+        loginWithSocial(providerToken!);
       } else {
-        // Step 9: Handle the error when idToken is null
         print(
             'Failed to retrieve idToken. The Google Sign-In might have failed.');
-        // You can also show a Snackbar or AlertDialog to inform the user
         Get.snackbar(
             'Error', 'Failed to sign in with Google. Please try again.');
       }
     } catch (e, stackTrace) {
-      // Step 10: Catch and handle any errors during the process
       isLoading.value = false;
       print("Google Login Failed: $e\n$stackTrace");
-      // You can also show a Snackbar or AlertDialog to inform the user
       Get.snackbar('Error',
           'An error occurred during Google Sign-In. Please try again.');
     } finally {
-      // Ensure that isLoading is set to false when the process is complete
       isLoading.value = false;
     }
   }
 
-  static const String emptyFirstNameError = 'First name required!';
-  static const String emptyLastNameError = 'Last name required!';
-  static const String invalidEmailError = 'Enter a valid email';
-  static const String shortPasswordError =
-      'Password must be at least 6 characters';
-  static const String passwordMismatchError = 'Passwords do not match';
+  Future<void> _initFCMToken() async {
+    messaging = FirebaseMessaging.instance;
 
-  ApiService apiService = Get.find();
-
-  Future<void> RequestIOSNotifications() async {
-    AwesomeNotifications().isNotificationAllowed().then((isAllowed) {
-      if (!isAllowed) {
-        // Insert here your friendly dialog box before call the request method
-        // This is very important to not harm the user experience
-        messaging = FirebaseMessaging.instance;
-        messaging!.getToken().then((value) {
-          print('token fcm ' + value.toString());
-          fcmToken = value.toString();
-          //PushToken(value!);
-          // addTokenToDatabase(userData!.fullName ?? '', value!);
-        });
-        AwesomeNotifications().requestPermissionToSendNotifications();
+    // توليد الـ FCM Token
+    messaging!.getToken().then((value) {
+      if (value != null) {
+        fcmToken = value;
+        print('FCM Token: $fcmToken');
+      } else {
+        print('Failed to generate FCM Token.');
       }
     });
-  }
 
-  String fcmToken = '';
-
-  FirebaseMessaging? messaging;
-
-  @override
-  void onInit() {
-    super.onInit();
-    RequestIOSNotifications();
-    messaging = FirebaseMessaging.instance;
-    messaging!.getToken().then((value) {
-      print('token fcm ' + value.toString());
-      fcmToken = value.toString();
-      //PushToken(value!);
-      // addTokenToDatabase(userData!.fullName ?? '', value!);
+    messaging!.onTokenRefresh.listen((newToken) {
+      fcmToken = newToken;
+      print('FCM Token refreshed: $fcmToken');
     });
-    socialView.value = true;
   }
+
+  Future<void> appleLogin() async {
+    try {
+      isLoading.value = true;
+
+      // Step 1: Get the credential from Apple
+      final credential = await SignInWithApple.getAppleIDCredential(
+        scopes: [
+          AppleIDAuthorizationScopes.email,
+          AppleIDAuthorizationScopes.fullName,
+        ],
+      );
+
+      if (credential.identityToken != null) {
+        // Step 2: Use the credential to sign in with Firebase or call your API
+        final providerToken = credential.identityToken;
+
+        // Example of using the credential with Firebase (if using Firebase Auth)
+        final oauthCredential = authTest.OAuthProvider('apple.com').credential(
+          idToken: providerToken,
+          accessToken: credential.authorizationCode,
+        );
+
+        final userCredential = await auth.signInWithCredential(oauthCredential);
+
+        email.value = userCredential.user!.email ?? '';
+        firstName.value = userCredential.user!.displayName ?? '';
+        lastName.value = ''; // يمكن تعديلها حسب الحاجة
+
+        // استدعاء API الخاص بك
+        loginWithSocial(providerToken!);
+      } else {
+        print(
+            'Failed to retrieve identityToken. The Apple Sign-In might have failed.');
+        Get.snackbar(
+            'Error', 'Failed to sign in with Apple. Please try again.');
+      }
+    } catch (e, stackTrace) {
+      isLoading.value = false;
+      print("Apple Login Failed: $e\n$stackTrace");
+      Get.snackbar(
+          'Error', 'An error occurred during Apple Sign-In. Please try again.');
+    } finally {
+      isLoading.value = false;
+    }
+  }
+
+  String invalidEmailError = 'Enter a valid email';
 
   bool validateEmail() {
     if (!GetUtils.isEmail(email.value)) {
@@ -155,44 +208,7 @@ class AuthController extends GetxController {
     return true;
   }
 
-  bool validatePassword() {
-    if (password.value.length < 6) {
-      passwordError.value = shortPasswordError;
-      return false;
-    }
-    passwordError.value = '';
-    return true;
-  }
-
-  bool validateConfirmPassword() {
-    if (confirmPassword.value != password.value) {
-      confirmPasswordError.value = passwordMismatchError;
-      return false;
-    }
-    confirmPasswordError.value = '';
-    return true;
-  }
-
-  bool validateFirstName() {
-    if (firstName.value.isEmpty) {
-      firstNameError.value = emptyFirstNameError;
-      return false;
-    }
-    firstNameError.value = '';
-    return true;
-  }
-
-  bool validateLastName() {
-    if (lastName.value.isEmpty) {
-      lastNameError.value = emptyLastNameError;
-      return false;
-    }
-    lastNameError.value = '';
-    return true;
-  }
-
-  void loginWithGoogle(String providerToken) async {
-    // Validate that all required data is available
+  void loginWithSocial(String providerToken) async {
     if (validateEmail()) {
       isLoading.value = true;
       globalController.errorMessage.value = '';
@@ -202,58 +218,39 @@ class AuthController extends GetxController {
         'email': email.value,
         'first_name': firstName.value,
         'last_name': lastName.value,
-        'imei': '1234', // Example IMEI, adjust as needed
-        'device_token': fcmToken, // FCM token for push notifications
-        'device_type': 'ios', // Change to 'android' if applicable
+        'imei': deviceId, // استخدام الجهاز الديناميكي الفريد
+        'device_token': fcmToken, // FCM token للإشعارات
+        'device_type': GetPlatform.isAndroid ? 'android' : 'ios',
       });
 
       try {
         final response = await apiConsumer.post(
-          'login/google', // Adjust the endpoint as needed
+          'login/google',
           formDataIsEnabled: true,
           formData: formData,
         );
 
         final apiResponse = ApiResponse.fromJson(response);
         if (apiResponse.status == 'success') {
-          print('Login successful');
           isGuest.value = false;
-
           await cacheUserData(apiResponse.data!);
           AppConstants.userData = apiResponse.data!;
           user.value = apiResponse.data!.user;
           userToken = AppConstants.userData!.token;
-
           Get.offNamedUntil(Routes.MAIN, (Route) => false);
         } else {
           handleApiErrorUser(apiResponse.message);
           handleApiError(response.statusCode);
           print('Login failed: ${response.statusMessage}');
         }
-        isLoading.value = false;
       } catch (e, stackTrace) {
         isGuest.value = false;
         isLoading.value = false;
-
         print('Login failed: $e $stackTrace');
         final apiResponse = ApiResponse.fromJson(jsonDecode(e.toString()));
         handleApiErrorUser(apiResponse.message);
-        print(e.toString() + stackTrace.toString());
       }
     }
-  }
-
-  void clearFields() {
-    email.value = '';
-    password.value = '';
-    confirmPassword.value = '';
-    firstName.value = '';
-    lastName.value = '';
-    emailError.value = '';
-    passwordError.value = '';
-    confirmPasswordError.value = '';
-    firstNameError.value = '';
-    lastNameError.value = '';
   }
 
   void register() async {
@@ -271,9 +268,9 @@ class AuthController extends GetxController {
         'email': email.value,
         'password': password.value,
         'password_confirmation': confirmPassword.value,
-        'imei': '1234',
-        'token': fcmToken == null || fcmToken.isEmpty ? 'test' : fcmToken,
-        'device_type': 'ios',
+        'imei': deviceId, // استخدام معرف الجهاز الديناميكي الفريد
+        'token': fcmToken.isEmpty ? 'test' : fcmToken, // FCM token
+        'device_type': GetPlatform.isAndroid ? 'android' : 'ios',
       });
       try {
         final response = await apiConsumer.post(
@@ -285,14 +282,12 @@ class AuthController extends GetxController {
         final apiResponse = ApiResponse.fromJson(response);
         if (apiResponse.status == 'success') {
           print('Registration successful');
-          // Handle successful registration
           isGuest.value = false;
 
           await cacheUserData(apiResponse.data!);
           AppConstants.userData = apiResponse.data!;
           user.value = apiResponse.data!.user;
           userToken = AppConstants.userData!.token;
-          // Get.off(() => MainView());
           Get.offNamedUntil(Routes.MAIN, (Route) => false);
         } else {
           handleApiErrorUser(apiResponse.message);
@@ -302,28 +297,80 @@ class AuthController extends GetxController {
         isLoading.value = false;
       } catch (e, stackTrace) {
         isGuest.value = false;
-
         isLoading.value = false;
         print('Registration failed:  ${e} $stackTrace');
-
+        print('Registration failed:  ${e} $stackTrace');
         final apiResponse = ApiResponse.fromJson(jsonDecode(e.toString()));
-
         handleApiErrorUser(apiResponse.message);
-        print(e.toString() + stackTrace.toString());
       }
     }
   }
 
+  void clearFields() {
+    email.value = '';
+    password.value = '';
+    confirmPassword.value = '';
+    firstName.value = '';
+    lastName.value = '';
+    emailError.value = '';
+    passwordError.value = '';
+    confirmPasswordError.value = '';
+    firstNameError.value = '';
+    lastNameError.value = '';
+  }
+
+  static const String shortPasswordError =
+      'Password must be at least 6 characters';
+
+  bool validatePassword() {
+    if (password.value.length < 6) {
+      passwordError.value = shortPasswordError;
+      return false;
+    }
+    passwordError.value = '';
+    return true;
+  }
+
+  bool validateConfirmPassword() {
+    if (confirmPassword.value != password.value) {
+      confirmPasswordError.value = 'Passwords do not match';
+      return false;
+    }
+    confirmPasswordError.value = '';
+    return true;
+  }
+
+  bool validateFirstName() {
+    if (firstName.value.isEmpty) {
+      firstNameError.value = 'First name required!';
+      return false;
+    }
+    firstNameError.value = '';
+    return true;
+  }
+
+  bool validateLastName() {
+    if (lastName.value.isEmpty) {
+      lastNameError.value = 'Last name required!';
+      return false;
+    }
+    lastNameError.value = '';
+    return true;
+  }
+
   void login() async {
+    getDeviceId();
+    print(deviceId.toString() + 'test device imemi');
+    print(fcmToken.toString() + 'test token fcm');
     if (validateEmail() && validatePassword()) {
       isLoading.value = true;
       globalController.errorMessage.value = '';
       var formData = dio.FormData.fromMap({
         'email': email.value,
         'password': password.value,
-        'token': fcmToken == null || fcmToken.isEmpty ? 'test' : fcmToken,
-        'imei': '1234',
-        'device_type': 'ios',
+        'token': fcmToken.isEmpty ? 'test' : fcmToken, // FCM token
+        'imei': deviceId, // استخدام معرف الجهاز الديناميكي الفريد
+        'device_type': GetPlatform.isAndroid ? 'android' : 'ios',
       });
       try {
         final response = await apiConsumer.post(
@@ -335,34 +382,23 @@ class AuthController extends GetxController {
         isLoading.value = false;
         final apiResponse = ApiResponse.fromJson(response);
         if (apiResponse.status == 'success') {
-          print("is fcm token is sent? ${fcmToken}");
-          print("my sent form data ${formData.fields}");
-          print('Login successful');
-          // Handle successful login
+          print("Login successful");
           isGuest.value = false;
 
           await cacheUserData(apiResponse.data!);
           AppConstants.userData = apiResponse.data!;
           user.value = apiResponse.data!.user;
           userToken = AppConstants.userData!.token;
-          // Get.off(() => MainView());
           Get.offNamedUntil(Routes.MAIN, (Route) => false);
         } else {
-          isGuest.value = false;
-
           handleApiErrorUser(apiResponse.message);
           handleApiError(response.statusCode);
         }
-        // Handle successful login
       } catch (e, stackTrace) {
         isLoading.value = false;
         isGuest.value = false;
-
         print('Login failed: ${e}');
-        print(e.toString() + stackTrace.toString());
-
         final apiResponse = ApiResponse.fromJson(jsonDecode(e.toString()));
-
         handleApiErrorUser(apiResponse.message);
       }
     }
@@ -370,7 +406,7 @@ class AuthController extends GetxController {
 
   Future<void> cacheUserData(UserData data) async {
     final prefs = await SharedPreferences.getInstance();
-    final userDataString = jsonEncode(data.toJson()); // Use jsonEncode
+    final userDataString = jsonEncode(data.toJson());
     await prefs.setString('user_data', userDataString);
     print('User data cached: $userDataString');
   }
