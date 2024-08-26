86082af5cfa28db6dafc4f06af79daef8b225186
MahmoudKirax677 <engm2056@gmail.com>
2024-08-26 20:38:19 +0300

    fix categroy bar

--- lib/app/modules/global/config/configs.dart
+++ lib/app/modules/global/config/configs.dart
@@ -6,7 +6,7 @@ const APP_NAME = 'Maryana';
 var defaultPrimaryColor = Color(0xFFD4B0FF);
 
 const DOMAIN_URL =
-    'https://mariana.genixarea.pro'; // Don't add slash at the end of the url
+    'https://panel.mariannella.com'; // Don't add slash at the end of the url
 const BASE_URL = '$DOMAIN_URL/api/';
 
 const DEFAULT_LANGUAGE = 'en';
